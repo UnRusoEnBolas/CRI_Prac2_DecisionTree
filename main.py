0a8d046b99957e29d4868013c8d6efb9a23542ed
UnRusoEnBolas <jgimenezp99@gmail.com>
2020-11-09 12:08:44 +0100

    .

--- main.py
+++ main.py
@@ -2,27 +2,12 @@ from models.DecisionTree import DecisionTree
 import pandas as pd
 pd.options.mode.chained_assignment = None
 
-'''
-testDataset = pd.read_csv('data/testDataset/continuousTestData.csv')
-yColumn = 'Decision'
-
-sa = C4_5SplittingAlgorithm(testDataset, yColumn, trueLabel='Yes')
-print(sa.getSplittingAttribute())
-
-print("------------------------")
-
 testDataset = pd.read_csv('data/testDataset/categoricalTestData.csv')
 yColumn = 'Decision'
+trueValue = 'Yes'
 
-sa = ID3SplittingAlgorithm(testDataset, yColumn, trueLabel='Yes')
-print(sa.getSplittingAttribute())
-'''
-
-testDataset = pd.read_csv('data/testDataset/categoricalTestData.csv')
-yColumn = 'Decision'
-
-decisionTreeID3 = DecisionTree(testDataset, yColumn, 'Yes', 'ID3')
+decisionTreeID3 = DecisionTree(testDataset, yColumn, trueValue, 'ID3')
 decisionTreeID3.generate()
 
-decisionTreeC45 = DecisionTree(testDataset, yColumn, 'Yes', 'C4.5')
+decisionTreeC45 = DecisionTree(testDataset, yColumn, trueValue, 'C4.5')
 decisionTreeC45.generate()
